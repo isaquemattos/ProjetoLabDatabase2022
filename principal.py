1fbe364b988240982a48fc75ac3a3a07a7e5b4a5
Daniel Marinho Ferreira de Souza <daniel.marinho.ferreira1@gmail.com>
2022-10-20 22:05:03 -0300

    realizado alhumas alteraçoes

--- principal.py
+++ principal.py
@@ -22,65 +22,6 @@ def run():
     print(tela_inicial.get_updated_screen())
     config.clear_console()
 
-    while True:
-        print(config.MENU_PRINCIPAL)
-        opcao = int(input("Escolha uma opção [1-5]: "))
-        config.clear_console(1)
-        
-        if opcao == 1: # Relatórios
-            
-            print(config.MENU_RELATORIOS)
-            opcao_relatorio = int(input("Escolha uma opção [0-6]: "))
-            config.clear_console(1)
-
-            #reports(opcao_relatorio)
-
-            config.clear_console(1)
-
-        elif opcao == 2: # Inserir Novos Registros
-            
-            print(config.MENU_ENTIDADES)
-            opcao_inserir = int(input("Escolha uma opção [1-5]: "))
-            config.clear_console(1)
-
-            #inserir(opcao_inserir=opcao_inserir)
-
-            config.clear_console()
-            print(tela_inicial.get_updated_screen())
-            config.clear_console()
-
-        elif opcao == 3: # Atualizar Registros
-
-            print(config.MENU_ENTIDADES)
-            opcao_atualizar = int(input("Escolha uma opção [1-5]: "))
-            config.clear_console(1)
-
-            #atualizar(opcao_atualizar=opcao_atualizar)
-
-            config.clear_console()
-
-        elif opcao == 4:
-
-            print(config.MENU_ENTIDADES)
-            opcao_excluir = int(input("Escolha uma opção [1-5]: "))
-            config.clear_console(1)
-
-            #excluir(opcao_excluir=opcao_excluir)
-
-            config.clear_console()
-            print(tela_inicial.get_updated_screen())
-            config.clear_console()
-
-        elif opcao == 5:
-
-            print(tela_inicial.get_updated_screen())
-            config.clear_console()
-            print("Obrigado por utilizar o nosso sistema.")
-            exit(0)
-
-        else:
-            print("Opção incorreta.")
-            exit(1)
-
+   
 if __name__ == "__main__":
     run()
